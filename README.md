157440875fb8900db8a9627d3af19f9242dc1b92
chenjt2001 <chenjintao2001@gmail.com>
2020-12-10 22:50:43 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FileManager
 基于python flask的web文件管理器
+
+## 使用步骤
+1、修改instance/config.py中的DATA_DIR值为想浏览文件的目录，PHOTO_DIR为想浏览照片的目录  
+2、`python FileManager.py`  
+3、访问http://127.0.0.1:5000/
+
+## 提示
+1、需前往http://127.0.0.1:5000/auth/register 界面注册后登录才能用  
+2、http://127.0.0.1:5000/file/explorer/ 浏览文件，http://127.0.0.1:5000/gallery 浏览照片
--- instance/config.py
+++ instance/config.py
@@ -10,7 +10,7 @@ SEND_FILE_MAX_AGE_DEFAULT = 0# Default: datetime.timedelta(hours=12)
 TEMP_PATH = 'temp'# 临时文件夹
 ################################################################################
 #file
-DATA_DIR = r'C:\Users\珒陶\OneDrive\HONOR Magic-link'#'D:/OneDrive/' # 要浏览的数据文件夹，需要绝对路径
+DATA_DIR = r'D:\OneDrive\HONOR Magic-link'#'D:/OneDrive/' # 要浏览的数据文件夹，需要绝对路径
 ################################################################################
 #gallery
-PHOTO_DIR = r'C:\Users\珒陶\OneDrive\HONOR Magic-link\Backup\Photos'
+PHOTO_DIR = r'D:\OneDrive\HONOR Magic-link\Backup\Photos'
